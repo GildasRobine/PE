6a4fb25d694b28165a3e310cd45dd9c0a4bf707a
Gildas <gildas.robine@wanadoo.fr>
2019-03-08 10:42:18 +0100

    Functions set, reset & flip
    Ajouts des 3 fonctions de fautes monobit + main

--- setbit.py
+++ setbit.py
@@ -1,29 +1,63 @@
-fileRead = open("instruction.txt","r")
-data = fileRead.read()
-#print(data)
-index = data.find(":")
+def setbit1(fileWrite,dataTemplate,instrBin,indexWrite):
+    fileWrite.write(dataTemplate[:indexWrite - 1])
+    for i in range(15, -1, -1):
+        instrFault = instrBin[:i] + "1" + instrBin[i + 1:]
+        if instrFault != instrBin:
+            fileWrite.write(bytes.fromhex(hex(int(instrFault, 2))[4:6]))
+            fileWrite.write(bytes.fromhex(hex(int(instrFault, 2))[2:4]))
+            indexWrite += 2;
+    fileWrite.write(dataTemplate[indexWrite - 1:])
+    return indexWrite
 
-instrHex = data[index+2:index+6]
-#print(instrHex)
-instrBin = bin(int(instrHex,16))[2:].zfill(16)
-#print(instrBin)
-fileRead.close()
+def resetbit(fileWrite,dataTemplate,instrBin,indexWrite):
+    fileWrite.write(dataTemplate[:indexWrite - 1])
+    for i in range(15, -1, -1):
+        instrFault = instrBin[:i] + "0" + instrBin[i + 1:]
 
-fileWrite = open("hexToArm.elf","wb")
-fileTemplate = open("template.elf","rb")
-dataTemplate = fileTemplate.read()
-indexWrite = 59
+        if instrFault != instrBin:
+            #print(instrFault + " : " + instrBin)
+            fileWrite.write(bytes.fromhex(("%04x" % int(instrFault,2))[2:4]))
+            fileWrite.write(bytes.fromhex(("%04x" % int(instrFault,2))[0:2]))
+            indexWrite += 2;
+    fileWrite.write(dataTemplate[indexWrite - 1:])
+    return indexWrite
 
-fileWrite.write(dataTemplate[:indexWrite-1])
-for i in range(15,-1,-1):
-    instrFault = instrBin[:i]+"1"+instrBin[i+1:]
-    if instrFault != instrBin:
-        fileWrite.write(bytes.fromhex(hex(int(instrFault,2))[4:6]))
-        fileWrite.write(bytes.fromhex(hex(int(instrFault, 2))[2:4]))
-        indexWrite += 2;
-fileWrite.write(dataTemplate[indexWrite-1:])
-fileWrite.close()
-fileTemplate.close()
-print(hex(indexWrite-53))
+def flipbit(fileWrite,dataTemplate,instrBin,indexWrite):
+    fileWrite.write(dataTemplate[:indexWrite - 1])
+    for i in range(15, -1, -1):
+        instrFault = instrBin[:i] + str(int(not int(instrBin[i]))) + instrBin[i + 1:]
+        #print(instrFault  +" : "+ instrBin)
+        if instrFault != instrBin:
+            #print(instrFault + " : " + instrBin)
+            fileWrite.write(bytes.fromhex(("%04x" % int(instrFault,2))[2:4]))
+            fileWrite.write(bytes.fromhex(("%04x" % int(instrFault,2))[0:2]))
+            indexWrite += 2;
+    fileWrite.write(dataTemplate[indexWrite - 1:])
+    return indexWrite
 
 
+
+
+
+def main():
+    fileRead = open("instruction.txt", "r")
+    data = fileRead.read()
+    # print(data)
+    index = data.find(":")
+
+    instrHex = data[index + 2:index + 6]
+    # print(instrHex)
+    instrBin = bin(int(instrHex, 16))[2:].zfill(16)
+    # print(instrBin)
+    fileRead.close()
+
+    fileWrite = open("hexToArm.elf", "wb")
+    fileTemplate = open("template.elf", "rb")
+    dataTemplate = fileTemplate.read()
+    indexWrite = 59
+    indexWrite = flipbit(fileWrite,dataTemplate,instrBin,indexWrite)
+    fileWrite.close()
+    fileTemplate.close()
+    print(hex(indexWrite - 53))
+
+main()
